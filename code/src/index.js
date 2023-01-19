3a1c6482805c9c38912d058def1a3d157194bc15
Diogo Silva <diogovicente10@hotmail.com>
2023-01-19 13:12:17 +0000

    Sprites Done

--- code/src/index.js
+++ code/src/index.js
@@ -2,52 +2,158 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class TriangleSprite {
+  constructor(x, y, gridSize, cellSize, triangleSize, color) {
+      this.x = x;
+      this.y = y;
+      this.gridSize = gridSize;
+      this.cellSize = cellSize;
+      this.triangleSize = triangleSize;
+      this.color = color;
+  }
+
+  draw(context) {
+    context.strokeStyle = this.color;
+    context.beginPath();
+    context.moveTo(
+        this.x * this.cellSize + this.cellSize / 2,
+        this.y * this.cellSize + this.cellSize / 2 - this.triangleSize / 2
+    );
+    context.lineTo(
+        this.x * this.cellSize + this.cellSize / 2 - this.triangleSize / 2,
+        this.y * this.cellSize + this.cellSize / 2 + this.triangleSize / 2
+    );
+    context.lineTo(
+        this.x * this.cellSize + this.cellSize / 2 + this.triangleSize / 2,
+        this.y * this.cellSize + this.cellSize / 2 + this.triangleSize / 2
+    );
+    context.lineTo(
+      this.x * this.cellSize + this.cellSize / 2,
+      this.y * this.cellSize + this.cellSize / 2 - this.triangleSize / 2
+    );
+    context.stroke();
+    context.closePath();
+  }
+}
+
+const triangleSprites = [];
+
+function drawTriangleSprites(context) {
+    triangleSprites.forEach(function(triangleSprite) {
+        triangleSprite.draw(context);
+    });
+}
+
+
 class SquareSprite {
-    constructor(x, y, gridSize, cellSize, squareSize) {
+    constructor(x, y, gridSize, cellSize, squareSize, color) {
       this.x = x;
       this.y = y;
       this.gridSize = gridSize;
       this.cellSize = cellSize;
       this.squareSize = squareSize;
+      this.color = color;
     }
   
     draw(context) {
-      context.fillRect(
-        this.x * this.cellSize + this.cellSize / 2,
-        this.y * this.cellSize + this.cellSize / 2,
+      context.strokeStyle = this.color;
+      context.beginPath();
+      context.strokeRect(
+        this.x * this.cellSize + (this.cellSize - this.squareSize) / 2,
+        this.y * this.cellSize + (this.cellSize - this.squareSize) / 2,
         this.squareSize,
         this.squareSize
       );
+      context.stroke()
+      context.closePath();
     }
   }
   
-  const sprites = [];
-  
-  function drawSprites(context) {
-      sprites.forEach(function(sprite) {
-          sprite.draw(context);
-      });
-  }
+const squareSprites = [];
+
+function drawsquareSprites(context) {
+    squareSprites.forEach(function(sprite) {
+        sprite.draw(context);
+    });
+}
   
 
-class CircleSprite {
-    constructor(x, y, gridSize, cellSize) {
+class StarSprite {
+  constructor(x, y, gridSize, cellSize, starSize, color) {
       this.x = x;
       this.y = y;
       this.gridSize = gridSize;
       this.cellSize = cellSize;
+      this.starSize = starSize;
+      this.color = color;
+      this.angle = Math.PI / 5;
+      this.ratio = 5 / 2;
+      this.innerRadius = this.starSize / this.ratio;
+  }
+
+  draw(context) {
+    context.strokeStyle = this.color;
+    //Draw the star
+    context.beginPath();
+    context.moveTo(
+      this.x * this.cellSize + this.cellSize / 2 + this.starSize * Math.cos(0 * 72 * Math.PI / 180 + Math.PI / 2),
+      this.y * this.cellSize + this.cellSize / 2 + this.starSize * Math.sin(0 * 72 * Math.PI / 180 + Math.PI / 2)
+    );
+    for (let i = 0; i <= 4; i++) {
+        context.lineTo(
+            this.x * this.cellSize + this.cellSize / 2 + this.starSize * Math.cos(i * 72 * Math.PI / 180 + Math.PI / 2),
+            this.y * this.cellSize + this.cellSize / 2 + this.starSize * Math.sin(i * 72 * Math.PI / 180 + Math.PI / 2)
+        );
+        context.lineTo(
+            this.x * this.cellSize + this.cellSize / 2 + this.starSize / 2 * Math.cos(i * 72 * Math.PI / 180 + Math.PI / 2 + 36 * Math.PI / 180),
+            this.y * this.cellSize + this.cellSize / 2 + this.starSize / 2 * Math.sin(i * 72 * Math.PI / 180 + Math.PI / 2 + 36 * Math.PI / 180)
+        );
+    }
+    context.closePath();
+    context.stroke();
+    // Draw the pentagon
+    context.beginPath();
+    context.save();
+    context.translate(
+      this.x * this.cellSize + this.cellSize / 2,
+      this.y * this.cellSize + this.cellSize / 2
+    );
+    context.rotate(55 * Math.PI / 180);
+    for (let i = 0; i <= 4; i++) {
+      context.lineTo(
+          this.starSize / 2 * Math.cos(i * 72 * Math.PI / 180),
+          this.starSize / 2 * Math.sin(i * 72 * Math.PI / 180)
+      );
+    }
+    context.closePath();
+    context.stroke();
+    context.restore();
+  }
+}
+
+
+class CircleSprite {
+    constructor(x, y, gridSize, cellSize, circleSize, color) {
+      this.x = x;
+      this.y = y;
+      this.gridSize = gridSize;
+      this.cellSize = cellSize;
+      this.circleSize = circleSize
+      this.color = color;
     }
   
     draw(context) {
+      context.strokeStyle = this.color;
       context.beginPath();
       context.arc(
         this.x * this.cellSize + this.cellSize / 2,
         this.y * this.cellSize + this.cellSize / 2,
-        this.cellSize / 2,
+        this.circleSize / 2,
         0,
         2 * Math.PI
       );
-      context.fill();
+      context.stroke();
+      context.closePath();
     }
   }
   
@@ -60,15 +166,6 @@ class CanvasWrapper extends React.Component {
       const gridSize = 10;
       const cellSize = canvas.width / gridSize;
 
-      
-      const circleSprite = new CircleSprite(3, 4, gridSize, cellSize);
-      circleSprite.draw(ctx);
-
-      sprites.push(new SquareSprite(3, 4, gridSize, cellSize, 10));
-      sprites.push(new SquareSprite(5, 6, gridSize, cellSize, 10));
-      drawSprites(ctx);
-
-      
       for (let i = 0; i <= gridSize; i++) {
         ctx.moveTo(i * cellSize, 0);
         ctx.lineTo(i * cellSize, canvas.height);
@@ -80,6 +177,23 @@ class CanvasWrapper extends React.Component {
         ctx.lineTo(canvas.width, i * cellSize);
         ctx.stroke();
       }
+      
+      const circleSprite = new CircleSprite(3, 4, gridSize, cellSize, cellSize/2 * 1.5, "blue");
+      circleSprite.draw(ctx);
+
+      const starSprite = new StarSprite(1, 2, gridSize, cellSize, cellSize/3, "yellow");
+      starSprite.draw(ctx);
+
+      const color = "green"
+      squareSprites.push(new SquareSprite(4, 4, gridSize, cellSize, cellSize/2 * 1.5, color));
+      squareSprites.push(new SquareSprite(5, 6, gridSize, cellSize, cellSize/2 * 1.5, color));
+      drawsquareSprites(ctx);
+
+      const colorT = "red"
+      triangleSprites.push(new TriangleSprite(6, 4, gridSize, cellSize, cellSize/3, colorT));
+      triangleSprites.push(new TriangleSprite(1, 6, gridSize, cellSize, cellSize/3, colorT));
+      drawTriangleSprites(ctx);
+     
     }
   
     render() {
